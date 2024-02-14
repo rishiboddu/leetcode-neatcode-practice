46d31763c689a0eb425a59bb3c46bf8c7534e794
rishi boddu <123269723+rishiboddu@users.noreply.github.com>
2024-02-14 12:12:46 -0800

    Update leetcode217.java

--- leetcode/leetcode217.java
+++ leetcode/leetcode217.java
@@ -1,6 +1,10 @@
 /* 
 https://leetcode.com/problems/contains-duplicate/description/
 leetcode 217: contains duplicates (easy)
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
 */
 
 import java.util.Map;
