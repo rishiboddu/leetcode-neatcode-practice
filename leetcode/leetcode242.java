ba36bcc47dfd5e23052a393cbc200d222c1c78d5
rishi boddu <123269723+rishiboddu@users.noreply.github.com>
2024-02-15 17:12:59 -0800

    Update leetcode242.java

--- leetcode/leetcode242.java
+++ leetcode/leetcode242.java
@@ -7,6 +7,9 @@ Given two strings s and t, return true if t is an anagram of s, and false otherw
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 s and t consist of lowercase English letters
 
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
 */
 
 
