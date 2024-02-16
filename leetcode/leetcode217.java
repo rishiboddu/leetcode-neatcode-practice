6fa85636473d01e9c4267551cb12e7153e5a4008
rishi boddu <123269723+rishiboddu@users.noreply.github.com>
2024-02-15 17:12:38 -0800

    Update leetcode217.java

--- leetcode/leetcode217.java
+++ leetcode/leetcode217.java
@@ -4,6 +4,8 @@ https://leetcode.com/problems/contains-duplicate/
 
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
 
 */
 
