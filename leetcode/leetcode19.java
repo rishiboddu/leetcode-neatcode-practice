77456209c7221ed93051a539b43896d46bd39864
rishi boddu <123269723+rishiboddu@users.noreply.github.com>
2024-02-17 03:43:59 -0800

    Update leetcode19.java

--- leetcode/leetcode19.java
+++ leetcode/leetcode19.java
@@ -1,5 +1,5 @@
 /*
-Leetcode #19. Remove Nth Node From End of List
+Leetcode #19: Remove Nth Node From End of List (medium)
 https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 
 Given the head of a linked list, remove the nth node from the end of the list and return its head.
